3af3c3e6440116b6979fcb30ccf964ddf6aa500f
jihad-hossain1 <jihadkhan934@gmail.com>
2023-11-01 17:59:41 +0600

    basic setup done

---
+++ app/(home)/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h4 className="text-center"> DCSC main_homepage</h4>
+    </div>
+  );
+};
+
+export default HomePage;
